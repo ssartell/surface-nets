458952f39905f7e34421d772b1502be619e3b9b9
ssartell <ssartell@gmail.com>
2019-03-31 08:10:46 -0500

    lod

--- Assets/ChunkManager.cs
+++ Assets/ChunkManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Assets;
 using UnityEngine;
 
 public class ChunkManager : MonoBehaviour
@@ -9,29 +11,54 @@ public class ChunkManager : MonoBehaviour
     public int ChunksZ = 1;
 
     public int ChunkSize = 32;
+    public int Lod = 2;
+    public float IsoLevel = 0f;
 
     public Material Material;
+
+    private Chunk[,,] _chunks;
+    private ChunkRenderer[,,] _chunkRenderers;
 
     // Start is called before the first frame update
     void Start()
     {
-        var size = this.ChunkSize - 1;  // chunks should overlap by one voxel
+        var sdf = GetSdf();
+
+        var size = this.ChunkSize;  // chunks should overlap by one voxel
         var offset = new Vector3(this.ChunksX, this.ChunksY, this.ChunksZ) * size / 2f;
+        _chunks = new Chunk[this.ChunksX, this.ChunksY, this.ChunksZ];
+        _chunkRenderers = new ChunkRenderer[this.ChunksX, this.ChunksY, this.ChunksZ];
+        var edges = EdgesTable();
+        var intersections = IntersectionsTable(edges);
+
         for (var x = 0; x < this.ChunksX; x++)
         {
             for (var y = 0; y < this.ChunksY; y++)
             {
                 for (var z = 0; z < this.ChunksZ; z++)
                 {
-                    var chunk = new GameObject();
+                    var index = new Vector3(x, y, z);
+                    var position = index * size - offset;
+                    var renderedChunk = new GameObject($"Chunk[{x},{y},{z}]");
 
-                    var renderer = chunk.AddComponent<MeshRenderer>();
+                    var chunk = GenerateChunk(sdf, position, index, ChunkSize, Lod);
+                    _chunks[x, y, z] = chunk;
+
+                    var renderer = renderedChunk.AddComponent<MeshRenderer>();
                     renderer.material = this.Material;
 
-                    var surfaceNets = chunk.AddComponent<SurfaceNets>();
+                    var chunkRenderer = renderedChunk.AddComponent<ChunkRenderer>();
+                    chunkRenderer.Chunk = chunk;
+                    chunkRenderer.Chunks = _chunks;
+                    chunkRenderer.Edges = edges;
+                    chunkRenderer.Intersections = intersections;
+                    chunkRenderer.ChunkRenderers = _chunkRenderers;
+                    chunkRenderer.IsoLevel = IsoLevel;
 
-                    chunk.transform.parent = transform;
-                    chunk.transform.position = new Vector3(x, y, z) * size - offset;
+                    renderedChunk.transform.parent = transform;
+                    renderedChunk.transform.position = position;
+
+                    _chunkRenderers[x, y, z] = chunkRenderer;
                 }
             }
         }
@@ -42,4 +69,130 @@ public class ChunkManager : MonoBehaviour
     {
         
     }
+
+    private Func<Vector3, float> GetSdf()
+    {
+        // wormy plane
+        //return Sdf.Intersection(
+        //        Sdf.Union(
+        //            Sdf.Plane(Vector3.up)
+        //                .Translate(new Vector3(0, 16, 0)),
+        //            Sdf.Sphere()
+        //                .Scale(8)
+        //                .Translate(Vector3.one * 16)),
+        //        Sdf.Perlin(this.Size / 5f))
+        //    .Transform(transform)
+        //    .ToFunc();
+
+        // wormy sphere
+        //return Sdf.Intersection(
+        //    Sdf.Sphere()
+        //        .Scale(40),
+        //    Sdf.Perlin(5))
+        //    .Transform(transform)
+        //    .ToFunc();
+
+        return Sdf.Sphere()
+            .Scale(40)
+            .Transform(transform)
+            .ToFunc();
+
+        // heart
+        //return Sdf.Sphere()
+        //    .Scale(15)
+        //    .Transform(p => new Vector3(
+        //        p.x, 
+        //        4 + 1.2f * p.y - Mathf.Abs(p.x) * Mathf.Sqrt((20 - Mathf.Abs(p.x)) / 20f), 
+        //        p.z * (2 - p.y / 15f)))
+        //    .Translate(Vector3.one * 20)
+        //    .Transform(transform)
+        //    .ToFunc();
+    }
+
+    private Chunk GenerateChunk(Func<Vector3, float> sdf, Vector3 position, Vector3 index, int chunkSize, int lod)
+    {
+        var size = chunkSize / lod;
+        var voxels = new Voxel[size + 1, size + 1, size + 1];
+        for (var x = 0; x <= size; x++)
+        {
+            for (var y = 0; y <= size; y++)
+            {
+                for (var z = 0; z <= size; z++)
+                {
+                    var p = new Vector3(x * lod, y * lod, z * lod);
+                    voxels[x, y, z] = new Voxel()
+                    {
+                        Position = p,
+                        Value = sdf(position + p)
+                    };
+                }
+            }
+        }
+
+        return new Chunk()
+        {
+            Position = position,
+            Index = index,
+            Size = chunkSize,
+            Lod = lod,
+            Voxels = voxels
+        }; ;
+    }
+
+    List<Edge> EdgesTable()
+    {
+        var edges = new List<Edge>();
+        var k = 0;
+        for (var i = 0; i < 8; ++i) // every corner
+        {
+            for (var j = 1; j <= 4; j <<= 1)    // adjacent corners
+            {
+                var p = i ^ j;
+                if (i <= p) // not already added
+                {
+                    edges.Add(new Edge()
+                    {
+                        i = i,
+                        j = p,
+                    });
+                }
+            }
+        }
+
+        return edges;
+    }
+
+    List<int> IntersectionsTable(List<Edge> edges)
+    {
+        var intersections = new List<int>();
+        for (int i = 0; i < 256; ++i)   // every combination of corners in or out of isosurface
+        {
+            int edgeMask = 0;
+            for (int j = 0; j < edges.Count; j++)    // every edge
+            {
+                var edge = edges[j];
+                var a = Convert.ToBoolean(i & (1 << edge.i));   // is first corner in?
+                var b = Convert.ToBoolean(i & (1 << edge.j));   // is second corner in?
+                edgeMask |= a != b ? (1 << j) : 0;   // if they are on different sides, add edge to mask
+            }
+            intersections.Add(edgeMask);
+        }
+
+        return intersections;
+    }
+}
+
+public struct Chunk
+{
+    public int Size;
+    public int Lod;
+    public Vector3 Position;
+    public Vector3 Index;
+    public Voxel[,,] Voxels;
+}
+
+public struct Voxel
+{
+    public float Value;
+    public Vector3 Position;
 }
---
+++ Assets/ChunkRenderer.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkRenderer : MonoBehaviour
+{
+    public float IsoLevel = 0f;
+    public Chunk Chunk;
+    public Chunk[,,] Chunks;
+    public ChunkRenderer[,,] ChunkRenderers;
+    public Cube[,,] Cubes;
+    public List<Edge> Edges;
+    public List<int> Intersections;
+
+    private readonly Corner[] _corners = {
+        new Corner(0,0,0),	// 0
+        new Corner(1,0,0), 	// 1
+        new Corner(0,0,1), 	// 2
+        new Corner(1,0,1), 	// 3
+        new Corner(0,1,0), 	// 4
+        new Corner(1,1,0), 	// 5
+        new Corner(0,1,1), 	// 6
+        new Corner(1,1,1)  	// 7
+    };
+
+    private List<Vector3> _vertices;
+    private List<int> _triangles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("starting surface nets");
+        _vertices = new List<Vector3>();
+        _triangles = new List<int>();
+
+        GenerateCubes();
+        GenerateMesh();
+
+        if (_vertices.Count > 65536)
+        {
+            Debug.LogWarning("Exceeded max vertex count of 65536 (2^16).");
+        }
+
+        var mesh = new Mesh
+        {
+            vertices = this._vertices.ToArray(),
+            triangles = this._triangles.ToArray()
+        };
+
+        mesh.RecalculateNormals();
+
+        gameObject.AddComponent<MeshFilter>().sharedMesh = mesh;
+        gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Cube GetCube(int x, int y, int z)
+    {
+        return Cubes[x / Chunk.Lod, x / Chunk.Lod, x / Chunk.Lod];
+    }
+
+    private void GenerateMesh()
+    {
+        var size = Cubes.GetLength(0);
+
+        for (var x = 0; x < size; x++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                for (var z = 0; z < size; z++)
+                {
+                    var cube = Cubes[x, y, z];
+                    if (!cube.IsOnSurface) continue;
+
+                    for (var i = 0; i < 3; i++)
+                    {
+                        if ((cube.EdgeMask & (1 << i)) == 0) continue;
+                        var v0 = 0;
+                        var v1 = 0;
+                        var v2 = 0;
+                        var v3 = 0;
+
+                        if (x * y * z != 0)
+                        {
+                            if (i == 0)
+                            {
+                                v0 = cube.VertexIndex;
+                                v1 = Cubes[x, y - 1, z].VertexIndex;
+                                v2 = Cubes[x, y - 1, z - 1].VertexIndex;
+                                v3 = Cubes[x, y, z - 1].VertexIndex;
+                            }
+                            else if (i == 1)
+                            {
+                                v0 = cube.VertexIndex;
+                                v1 = Cubes[x - 1, y, z].VertexIndex;
+                                v2 = Cubes[x - 1, y - 1, z].VertexIndex;
+                                v3 = Cubes[x, y - 1, z].VertexIndex;
+                            }
+                            else if (i == 2)
+                            {
+                                v0 = cube.VertexIndex;
+                                v1 = Cubes[x, y, z - 1].VertexIndex;
+                                v2 = Cubes[x - 1, y, z - 1].VertexIndex;
+                                v3 = Cubes[x - 1, y, z].VertexIndex;
+                            }
+
+                            if (cube.Value < IsoLevel)
+                            {
+                                _triangles.AddRange(new[] { v0, v1, v2 });
+                                _triangles.AddRange(new[] { v0, v2, v3 });
+                            }
+                            else
+                            {
+                                _triangles.AddRange(new[] { v0, v2, v1 });
+                                _triangles.AddRange(new[] { v0, v3, v2 });
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+    }
+
+    private void GenerateCubes()
+    {
+        var vertexIndex = 0;
+        var size = Chunk.Voxels.GetLength(0) - 1;
+        Cubes = new Cube[size, size, size];
+        for (var x = 0; x < size; x++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                for (var z = 0; z < size; z++)
+                {
+                    var cornerVoxels = new Voxel[8];
+                    var cornerMask = 0;
+                    for (var i = 0; i < 8; i++)
+                    {
+                        var corner = _corners[i];
+                        var voxel = Chunk.Voxels[x + corner.x, y + corner.y, z + corner.z];
+                        cornerVoxels[i] = voxel;
+                        cornerMask |= voxel.Value >= IsoLevel ? 1 << i : 0;
+                    }
+
+                    if (cornerMask == 0 || cornerMask == 0xff) continue; // all corners are inside or outside the surface
+
+                    var edgeMask = Intersections[cornerMask];
+
+                    var vertex = Vector3.zero;
+                    var crossings = 0;
+                    for (var i = 0; i < Edges.Count; i++)
+                    {
+                        if ((edgeMask & (1 << i)) == 0) continue;
+                        crossings++;
+
+                        var edge = Edges[i];
+                        var x0 = cornerVoxels[edge.i];
+                        var x1 = cornerVoxels[edge.j];
+                        var y0 = x0.Value;
+                        var y1 = x1.Value;
+
+                        var t = (IsoLevel - y0) / (y1 - y0);
+                        vertex += Vector3.Lerp(x0.Position, x1.Position, t);
+                    }
+
+                    vertex /= crossings;
+
+                    // minecraft-y
+                    //vertex = (cornerVoxels[0].Position + cornerVoxels[7].Position) / 2;
+
+                    var cube = new Cube()
+                    {
+                        CornerMask = cornerMask,
+                        IsOnSurface = cornerMask != 0 && cornerMask != 0xff,
+                        EdgeMask = Intersections[cornerMask],
+                        VertexPosition = vertex,
+                        VertexIndex = vertexIndex,
+                        Value = Chunk.Voxels[x, y, z].Value
+                    };
+
+                    Cubes[x, y, z] = cube;
+                    _vertices.Add(vertex);
+                    vertexIndex++;
+                }
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.white;
+        //Gizmos.DrawWireMesh(this.GetComponent<MeshFilter>().mesh, transform.localPosition);
+        var size = Cubes.GetLength(0);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(Vector3.one * Chunk.Size / 2f + transform.localPosition, transform.TransformVector(Vector3.one * Chunk.Size));
+
+        for (var x = 0; x < size; x++)
+        {
+            for (var y = 0; y < size; y++)
+            {
+                for (var z = 0; z < size; z++)
+                {
+                    if (Cubes[x, y, z].IsOnSurface)
+                    {
+                        Gizmos.DrawWireCube(new Vector3(x + .5f, y + .5f, z + .5f) * Chunk.Lod + transform.localPosition, Vector3.one * Chunk.Lod);
+                    }
+                }
+            }
+        }
+    }
+}
+
+public struct Cube
+{
+    public bool IsOnSurface;
+    public int EdgeMask;
+    public int CornerMask;
+    public Vector3 VertexPosition;
+    public int VertexIndex;
+    public float Value;
+}
+
+public struct Edge
+{
+    public int i;
+    public int j;
+}
+
+public struct Corner
+{
+    public int x;
+    public int y;
+    public int z;
+
+    public Corner(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
